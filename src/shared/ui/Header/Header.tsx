cd7ea034920656060f69ecfc376843714bb330dc
Valeria Yatsuk <proslera@yandex.ru>
2026-05-01 16:36:03 +0300

    chore: перенос компонента HEADER в папку shared

---
+++ src/shared/ui/Header/Header.tsx
@@ -0,0 +1,18 @@
+import IconLocation from '@/shared/ui/icons/IconLocation';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header__wrapper">
+                <span className="header__logo-text">Need for drive</span>
+                <div className="header__location">
+                    <IconLocation className="header__icon" />
+                    <span className="header__city">Ульяновск</span>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
